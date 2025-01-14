a815bc7cfd741daf9ff7a9e06ee3f3c2831c7f0d
adikgh <adiikgh@gmail.com>
2025-01-14 11:38:03 +0500

    1.0129

--- config/core.php
+++ config/core.php
@@ -60,7 +60,7 @@
 
    // setting
    $site = mysqli_fetch_array(db::query("select * from `site` where id = 1"));
-   $ver = 1.028;
+   $ver = 1.029;
    $site_set = [
       'menu' => true,
       'search' => true,
